7556c71ac45b858fd45a1eb9ca0c55273ea1069c
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-07-27 14:11:49 +0400

    feat: add express server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT);

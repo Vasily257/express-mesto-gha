32c7a5f9b2f0e65487110d4e811e8bf8d782b2a8
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-07-31 22:07:09 +0400

    feat: add error handlers when accepting card requests

--- src/controllers/cardControllers.js
+++ src/controllers/cardControllers.js
@@ -1,45 +1,106 @@
 const { Card } = require('../models/cardModels');
+const {
+  handleIncorrectDataError,
+  createNotFoundError,
+  handleNotFoundError,
+  handleDefaultError,
+} = require('../utils/utils');
 
 module.exports.getCards = async (req, res) => {
-  const cards = await Card.find({});
-  res.send(cards);
+  try {
+    const cards = await Card.find({});
+    res.send(cards);
+  } catch (err) {
+    handleDefaultError(err);
+  }
 };
 
 module.exports.createCard = async (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
 
-  const card = await Card.create({ name, link, owner });
+  try {
+    const card = await Card.create({ name, link, owner });
 
-  res.send(card);
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        handleIncorrectDataError(res, 'Переданы некорректные данные при создании карточки.');
+        break;
+      default:
+        handleDefaultError(err);
+    }
+  }
 };
 
 module.exports.deleteCard = async (req, res) => {
-  const card = await Card.findByIdAndDelete(req.params.id);
+  try {
+    const card = await Card.findByIdAndDelete(req.params.id).orFail(() =>
+      createNotFoundError('Карточка по указанному _id не найдена.')
+    );
 
-  res.send(card);
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'CastError':
+        handleIncorrectDataError(res, 'Неправильно указан _id карточки.');
+        break;
+      case 'DocumentNotFoundError':
+        handleNotFoundError(res, err);
+        break;
+      default:
+        handleDefaultError(err);
+    }
+  }
 };
 
 module.exports.likeCard = async (req, res) => {
-  const card = await Card.findByIdAndUpdate(
-    req.params.id,
-    {
-      $addToSet: { likes: req.user._id },
-    },
-    { new: true }
-  );
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.id,
+      {
+        $addToSet: { likes: req.user._id },
+      },
+      { new: true }
+    ).orFail(() => createNotFoundError('Карточка по указанному _id не найдена.'));
 
-  res.send(card);
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'CastError':
+        handleIncorrectDataError(res, 'Переданы некорректные данные для постановки/снятии лайка.');
+        break;
+      case 'DocumentNotFoundError':
+        handleNotFoundError(res, err);
+        break;
+      default:
+        handleDefaultError(err);
+    }
+  }
 };
 
 module.exports.dislikeCard = async (req, res) => {
-  const card = await Card.findByIdAndUpdate(
-    req.params.id,
-    {
-      $pull: { likes: req.user._id },
-    },
-    { new: true }
-  );
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: { likes: req.user._id },
+      },
+      { new: true }
+    ).orFail(() => createNotFoundError('Карточка по указанному _id не найдена.'));
 
-  res.send(card);
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'CastError':
+        handleIncorrectDataError(res, 'Переданы некорректные данные для постановки/снятии лайка.');
+        break;
+      case 'DocumentNotFoundError':
+        handleNotFoundError(res, err);
+        break;
+      default:
+        handleDefaultError(err);
+    }
+  }
 };

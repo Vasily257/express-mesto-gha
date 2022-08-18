0e52212890293c73f03aa6d8d68204ac184424f2
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-08-18 08:05:35 +0400

    feat: add the regex for link validaiton

--- src/middlewares/validate-requests.js
+++ src/middlewares/validate-requests.js
@@ -1,5 +1,8 @@
 const { celebrate, Joi } = require('celebrate');
 
+// eslint-disable-next-line no-useless-escape
+const linkRegex = /https?:\/\/(www\.)?([a-z0-9\-\.])+(\.\w*)([\w\-\._~:\/\?#\[\]@!\$&'\(\)\*\+,;=])*/i;
+
 const userNameRules = Joi.string().min(2).max(30);
 const aboutRules = Joi.string().min(2).max(30);
 const emailRules = Joi.string().required().email();
@@ -10,7 +13,7 @@ const cardOwnerRules = Joi.string().alphanum().length(24);
 const likesRules = Joi.array();
 const createdDateRules = Joi.date();
 
-const linkRules = Joi.string();
+const linkRules = Joi.string().regex(linkRegex);
 const idRules = Joi.string().alphanum().length(24);
 const authorizationRules = Joi.string().required().regex(/Bearer \w+/);
 
